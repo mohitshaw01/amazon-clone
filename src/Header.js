10680fad125c97bf5965133ab6cdfb4bba8d0458
mohti <mohitshawmath@gmail.com>
2022-04-09 13:44:47 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header.css"
+function Header() {
+  return (
+    <nav className='header'>
+   <img
+        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+            className="header__logo"
+            alt="Logo" />
+    </nav>
+  )
+}
+
+export default Header
